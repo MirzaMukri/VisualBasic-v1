3b5ae62bf80ba7bb20a15adb417370be1eabd6c2
Mirza Mukri <mukri@planetbn.com>
2017-04-07 11:50:24 +0800

    First Commit

---
+++ VBIntro/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.username = New System.Windows.Forms.TextBox
+        Me.password = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 214)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(101, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Remember Me?"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'username
+        '
+        Me.username.Location = New System.Drawing.Point(12, 27)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(100, 20)
+        Me.username.TabIndex = 1
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(13, 72)
+        Me.password.Name = "password"
+        Me.password.Size = New System.Drawing.Size(100, 20)
+        Me.password.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.username)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Name = "Form1"
+        Me.Text = "User Info"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents username As System.Windows.Forms.TextBox
+    Friend WithEvents password As System.Windows.Forms.TextBox
+
+End Class
---
+++ VBIntro/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If CheckBox1.Checked = True Then
+            My.Settings.User = username.Text
+            My.Settings.Pass = password.Text
+        Else
+            My.Settings.User = ""
+            My.Settings.Pass = ""
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        username.Text = My.Settings.User
+        password.Text = My.Settings.Pass
+    End Sub
+End Class
